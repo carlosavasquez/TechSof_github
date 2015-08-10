7dad4d476dac0921dbbfdcd110788c1aa19513a3
Carlos Vasquez <carlosvasquez89@hotmail.com>
2015-08-10 09:44:50 -0500

    aa

--- TechSoft/Capa Diseño/Menu Principal.Designer.vb
+++ TechSoft/Capa Diseño/Menu Principal.Designer.vb
@@ -22,31 +22,34 @@ Partial Class Menu_Principal
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Menu_Principal))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.ClientesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.btn_casos = New System.Windows.Forms.ToolStripMenuItem()
-        Me.HerramientasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.CambiarClaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.InformesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
-        Me.EquiposToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.btn_nuevocaso = New System.Windows.Forms.ToolStripMenuItem()
-        Me.btn_casospendientes = New System.Windows.Forms.ToolStripMenuItem()
-        Me.btn_editarcaso = New System.Windows.Forms.ToolStripMenuItem()
         Me.NuevoClienteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.EditarClienteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EquiposToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NuevoEquipoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.EditarEquipoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btn_casos = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btn_nuevocaso = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btn_editarcaso = New System.Windows.Forms.ToolStripMenuItem()
+        Me.btn_casospendientes = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HerramientasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CambiarClaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.MenuStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ClientesToolStripMenuItem, Me.EquiposToolStripMenuItem, Me.btn_casos, Me.HerramientasToolStripMenuItem, Me.InformesToolStripMenuItem, Me.CambiarClaveToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(762, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(762, 29)
         Me.MenuStrip1.TabIndex = 1
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -54,33 +57,82 @@ Partial Class Menu_Principal
         '
         Me.ClientesToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoClienteToolStripMenuItem, Me.EditarClienteToolStripMenuItem})
         Me.ClientesToolStripMenuItem.Name = "ClientesToolStripMenuItem"
-        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
+        Me.ClientesToolStripMenuItem.Size = New System.Drawing.Size(80, 25)
         Me.ClientesToolStripMenuItem.Text = "Clientes"
+        '
+        'NuevoClienteToolStripMenuItem
+        '
+        Me.NuevoClienteToolStripMenuItem.Name = "NuevoClienteToolStripMenuItem"
+        Me.NuevoClienteToolStripMenuItem.Size = New System.Drawing.Size(183, 26)
+        Me.NuevoClienteToolStripMenuItem.Text = "Nuevo Cliente"
+        '
+        'EditarClienteToolStripMenuItem
+        '
+        Me.EditarClienteToolStripMenuItem.Name = "EditarClienteToolStripMenuItem"
+        Me.EditarClienteToolStripMenuItem.Size = New System.Drawing.Size(183, 26)
+        Me.EditarClienteToolStripMenuItem.Text = "Editar Cliente"
+        '
+        'EquiposToolStripMenuItem
+        '
+        Me.EquiposToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoEquipoToolStripMenuItem, Me.EditarEquipoToolStripMenuItem})
+        Me.EquiposToolStripMenuItem.Name = "EquiposToolStripMenuItem"
+        Me.EquiposToolStripMenuItem.Size = New System.Drawing.Size(80, 25)
+        Me.EquiposToolStripMenuItem.Text = "Equipos"
+        '
+        'NuevoEquipoToolStripMenuItem
+        '
+        Me.NuevoEquipoToolStripMenuItem.Name = "NuevoEquipoToolStripMenuItem"
+        Me.NuevoEquipoToolStripMenuItem.Size = New System.Drawing.Size(183, 26)
+        Me.NuevoEquipoToolStripMenuItem.Text = "Nuevo Equipo"
+        '
+        'EditarEquipoToolStripMenuItem
+        '
+        Me.EditarEquipoToolStripMenuItem.Name = "EditarEquipoToolStripMenuItem"
+        Me.EditarEquipoToolStripMenuItem.Size = New System.Drawing.Size(183, 26)
+        Me.EditarEquipoToolStripMenuItem.Text = "Editar Equipo"
         '
         'btn_casos
         '
         Me.btn_casos.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.btn_nuevocaso, Me.btn_editarcaso, Me.btn_casospendientes})
         Me.btn_casos.Name = "btn_casos"
-        Me.btn_casos.Size = New System.Drawing.Size(50, 20)
+        Me.btn_casos.Size = New System.Drawing.Size(64, 25)
         Me.btn_casos.Text = "Casos"
+        '
+        'btn_nuevocaso
+        '
+        Me.btn_nuevocaso.Name = "btn_nuevocaso"
+        Me.btn_nuevocaso.Size = New System.Drawing.Size(234, 26)
+        Me.btn_nuevocaso.Text = "Nuevo Caso"
+        '
+        'btn_editarcaso
+        '
+        Me.btn_editarcaso.Name = "btn_editarcaso"
+        Me.btn_editarcaso.Size = New System.Drawing.Size(234, 26)
+        Me.btn_editarcaso.Text = "Editar Caso"
+        '
+        'btn_casospendientes
+        '
+        Me.btn_casospendientes.Name = "btn_casospendientes"
+        Me.btn_casospendientes.Size = New System.Drawing.Size(234, 26)
+        Me.btn_casospendientes.Text = "Ver Casos Pendientes"
         '
         'HerramientasToolStripMenuItem
         '
         Me.HerramientasToolStripMenuItem.Name = "HerramientasToolStripMenuItem"
-        Me.HerramientasToolStripMenuItem.Size = New System.Drawing.Size(90, 20)
+        Me.HerramientasToolStripMenuItem.Size = New System.Drawing.Size(120, 25)
         Me.HerramientasToolStripMenuItem.Text = "Herramientas"
-        '
-        'CambiarClaveToolStripMenuItem
-        '
-        Me.CambiarClaveToolStripMenuItem.Name = "CambiarClaveToolStripMenuItem"
-        Me.CambiarClaveToolStripMenuItem.Size = New System.Drawing.Size(96, 20)
-        Me.CambiarClaveToolStripMenuItem.Text = "Cambiar Clave"
         '
         'InformesToolStripMenuItem
         '
         Me.InformesToolStripMenuItem.Name = "InformesToolStripMenuItem"
-        Me.InformesToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.InformesToolStripMenuItem.Size = New System.Drawing.Size(88, 25)
         Me.InformesToolStripMenuItem.Text = "Informes"
+        '
+        'CambiarClaveToolStripMenuItem
+        '
+        Me.CambiarClaveToolStripMenuItem.Name = "CambiarClaveToolStripMenuItem"
+        Me.CambiarClaveToolStripMenuItem.Size = New System.Drawing.Size(125, 25)
+        Me.CambiarClaveToolStripMenuItem.Text = "Cambiar Clave"
         '
         'StatusStrip1
         '
@@ -90,54 +142,10 @@ Partial Class Menu_Principal
         Me.StatusStrip1.TabIndex = 2
         Me.StatusStrip1.Text = "StatusStrip1"
         '
-        'EquiposToolStripMenuItem
+        'ContextMenuStrip1
         '
-        Me.EquiposToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevoEquipoToolStripMenuItem, Me.EditarEquipoToolStripMenuItem})
-        Me.EquiposToolStripMenuItem.Name = "EquiposToolStripMenuItem"
-        Me.EquiposToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
-        Me.EquiposToolStripMenuItem.Text = "Equipos"
-        '
-        'btn_nuevocaso
-        '
-        Me.btn_nuevocaso.Name = "btn_nuevocaso"
-        Me.btn_nuevocaso.Size = New System.Drawing.Size(186, 22)
-        Me.btn_nuevocaso.Text = "Nuevo Caso"
-        '
-        'btn_casospendientes
-        '
-        Me.btn_casospendientes.Name = "btn_casospendientes"
-        Me.btn_casospendientes.Size = New System.Drawing.Size(186, 22)
-        Me.btn_casospendientes.Text = "Ver Casos Pendientes"
-        '
-        'btn_editarcaso
-        '
-        Me.btn_editarcaso.Name = "btn_editarcaso"
-        Me.btn_editarcaso.Size = New System.Drawing.Size(186, 22)
-        Me.btn_editarcaso.Text = "Editar Caso"
-        '
-        'NuevoClienteToolStripMenuItem
-        '
-        Me.NuevoClienteToolStripMenuItem.Name = "NuevoClienteToolStripMenuItem"
-        Me.NuevoClienteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.NuevoClienteToolStripMenuItem.Text = "Nuevo Cliente"
-        '
-        'EditarClienteToolStripMenuItem
-        '
-        Me.EditarClienteToolStripMenuItem.Name = "EditarClienteToolStripMenuItem"
-        Me.EditarClienteToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.EditarClienteToolStripMenuItem.Text = "Editar Cliente"
-        '
-        'NuevoEquipoToolStripMenuItem
-        '
-        Me.NuevoEquipoToolStripMenuItem.Name = "NuevoEquipoToolStripMenuItem"
-        Me.NuevoEquipoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.NuevoEquipoToolStripMenuItem.Text = "Nuevo Equipo"
-        '
-        'EditarEquipoToolStripMenuItem
-        '
-        Me.EditarEquipoToolStripMenuItem.Name = "EditarEquipoToolStripMenuItem"
-        Me.EditarEquipoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.EditarEquipoToolStripMenuItem.Text = "Editar Equipo"
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(61, 4)
         '
         'Menu_Principal
         '
@@ -147,11 +155,11 @@ Partial Class Menu_Principal
         Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.MenuStrip1)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
-        Me.IsMdiContainer = True
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "Menu_Principal"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Menu_Principal"
+        Me.Text = "Menu Principal"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.ResumeLayout(False)
@@ -173,4 +181,5 @@ Partial Class Menu_Principal
     Friend WithEvents btn_nuevocaso As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents btn_editarcaso As System.Windows.Forms.ToolStripMenuItem
     Friend WithEvents btn_casospendientes As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
 End Class
--- TechSoft/Capa Diseño/Menu Principal.vb
+++ TechSoft/Capa Diseño/Menu Principal.vb
@@ -5,9 +5,11 @@
     End Sub
 
     Private Sub btn_nuevocaso_Click(sender As Object, e As EventArgs) Handles btn_nuevocaso.Click
-        Dim ver As New Crear_Caso
-        ver.MdiParent = Me
+        Dim ver As New frmMSGctrl
         ver.Show()
+    End Sub
 
+    Private Sub CambiarClaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CambiarClaveToolStripMenuItem.Click
+        'Call Menudesp.activar("mensaje", 2, 3, True, "link", "error")
     End Sub
 End Class
